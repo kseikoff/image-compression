7ec4e6128baa5855fadbdbf02493f7e7917e83cc
Alexey <thelegendofflaity@gmail.com>
2023-12-20 17:02:48 +0300

    added rgb support

--- img_utils.py
+++ img_utils.py
@@ -17,16 +17,48 @@ def get_image_paths(folder_path: str):
 def read_image(path:str):
     return Image.open(path)
 
-def save_img(img:Image, path:str):
-    path_to_save = path.replace("images", "compr_images")
-    img.convert('RGB').save(path_to_save)
-    return path_to_save
+# using 'np.uint8(img)' causes some pixels to be missing
+def save_img(img:Image, path:str, convert_to:str):
+    img.convert(convert_to).save(path)
+
+def calc_compr_val(dim:int, compr_perc:int):
+    return dim - int(dim * (compr_perc / 100))
+
+def get_compressed_svd(img:Image, compr_perc:int):
+    img_arr = np.asarray(img)
+    return get_compressed_svd(img_arr, compr_perc)
+
+def get_compressed_svd(arr:list, compr_perc:int):
+    U, S, V = np.linalg.svd(arr)
+
+    compr_val = calc_compr_val(dim=S.shape[0], compr_perc=compr_perc)
+
+    return U[:, :compr_val], S[:compr_val], V[:compr_val, :]
 
 def compress_grayscale_img(img:Image, compr_perc:int):
-    gray_img_arr = np.asarray(img.convert('L'))
-    U, S, V = np.linalg.svd(gray_img_arr)
+    grayscale_img = img.convert('L')
+    U_compr, S_compr, V_compr = get_compressed_svd(grayscale_img, compr_perc)
 
-    dim_S = S.shape[0]
-    compr_val = dim_S - int(dim_S * (compr_perc / 100))
+    return Image.fromarray(np.matrix(U_compr) * np.diag(S_compr) * np.matrix(V_compr))
 
-    return Image.fromarray(np.matrix(U[:, :compr_val]) * np.diag(S[:compr_val]) * np.matrix(V[:compr_val, :]))
+def compress_rgb_img(img:Image, compr_perc:int):
+    red_img_arr = np.array(img)[:, :, 0]
+    green_img_arr = np.array(img)[:, :, 1]
+    blue_img_arr = np.array(img)[:, :, 2]
+
+    U_r_compr, S_r_compr, V_r_compr = get_compressed_svd(red_img_arr, compr_perc)
+    U_g_compr, S_g_compr, V_g_compr = get_compressed_svd(green_img_arr, compr_perc)
+    U_b_compr, S_b_compr, V_b_compr = get_compressed_svd(blue_img_arr, compr_perc)
+
+    red_img_arr_compr = np.matrix(U_r_compr) * np.diag(S_r_compr) * np.matrix(V_r_compr)
+    green_img_arr_compr = np.matrix(U_g_compr) * np.diag(S_g_compr) * np.matrix(V_g_compr)
+    blue_img_arr_compr = np.matrix(U_b_compr) * np.diag(S_b_compr) * np.matrix(V_b_compr)
+
+    rgb_compr_img_arr = np.zeros((np.array(img).shape[0], np.array(img).shape[1], 3))
+    rgb_compr_img_arr[:, :, 0] = red_img_arr_compr
+    rgb_compr_img_arr[:, :, 1] = green_img_arr_compr
+    rgb_compr_img_arr[:, :, 2] = blue_img_arr_compr
+    rgb_compr_img_arr = np.clip(rgb_compr_img_arr, 0, 255)
+    rgb_compr_img_arr = np.around(rgb_compr_img_arr).astype(int)
+
+    return Image.fromarray(rgb_compr_img_arr.astype(np.uint8))
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import ask_user
 import img_utils
 
+folder_path = "images"
+compr_folder_path = "compr_images"
+
 if __name__=="__main__":
-    png_imgs = img_utils.get_image_paths(folder_path='images')
+    png_imgs = img_utils.get_image_paths(folder_path)
     img_to_open = ask_user.ask_img_to_open(png_imgs)
     compr_perc = ask_user.ask_img_compression_percentage()
 
     print("Please, wait...")
     img_path = png_imgs[img_to_open - 1]
     img = img_utils.read_image(img_path)
-    compr_img = img_utils.compress_grayscale_img(img, compr_perc)
+    compr_img = img_utils.compress_rgb_img(img, compr_perc)
     
-    save_path = img_utils.save_img(compr_img, img_path)
-    if (save_path != None):
-        print(f"Done! Check compressed image at path {save_path}")
-    else:
-        print("We're sorry, something went wrong")
+    save_path = img_path.replace(folder_path, compr_folder_path)
+    img_utils.save_img(compr_img, save_path, convert_to='RGB')
+    print(f"Done! Check compressed image at path {save_path}")
